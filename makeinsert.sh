5d2b00f01e414c40cd4b7bde1b07944a98737c5d
Mark Karpeles <magicaltux@gmail.com>
2025-10-01 08:05:29 +0900

    improve makeinsert

--- makeinsert.sh
+++ makeinsert.sh
@@ -1,15 +1,13 @@
 #!/bin/sh
-echo >&2 "Run the following to include MCPClient in any nodejs:"
+echo >&2 "Run the following to include MCPClient in any nodejs with node --experimental-permission:"
+
+MCPJS="$(cat dist/mcp-client.js | jq -Rsa .)"
 
 cat <<EOF
 // Create a mock module system for eval
 const moduleWrapper = { exports: {} };
 (function(module, exports, require) {
-eval(
-EOF
-cat dist/mcp-client.js | jq -Rsa .
-cat <<EOF
-);
+eval($MCPJS);
 })(moduleWrapper, moduleWrapper.exports, require);
 
 const { MCPClient, createClients } = moduleWrapper.exports;
